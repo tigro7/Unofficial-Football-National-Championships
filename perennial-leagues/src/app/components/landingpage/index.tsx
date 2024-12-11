375a1927af25bfbfb418669dadbea723aa4b2d9d
tigro7 <alby.tiri@gmail.com>
2024-12-11 16:27:28 +0100

    bugfix landingpage

--- perennial-leagues/src/app/components/landingpage/index.tsx
+++ perennial-leagues/src/app/components/landingpage/index.tsx
@@ -1,8 +1,37 @@
 'use client';
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const LandingPage = () => {
+
+  const [nextMatchDate, setNextMatchDate] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNextMatch = async () => {
+      try {
+        const host = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'; // URL di base
+        const response = await fetch(`${host}/api/matches/serie_a/last`);
+        const data = (await response.json())[0];
+
+        // Format the date
+        const nextMatchDate = new Date(data.data)
+          .toLocaleDateString()
+          .split('/')
+          .reverse()
+          .join('-');
+        setNextMatchDate(nextMatchDate);
+      } catch (error) {
+        console.error("Error fetching next match:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchNextMatch();
+  }, []);
+
   return (
     <div className="container mx-auto mt-8 p-4">
       {/* Header con il link per tornare alla Home */}
@@ -15,10 +44,21 @@ const LandingPage = () => {
 
       {/* Contenuto principale */}
       <div className="flex flex-col items-center">
-        {/* Link al prossimo match */}
-        <Link href="/match/next/serie_a" className="w-full max-w-md p-6 mb-6 text-center bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition-all">
-            Prossima difesa del titolo
-        </Link>
+        {isLoading ? (
+          <p>Caricamento...</p>
+        ) : nextMatchDate ? (
+          <>
+            {/* Link al prossimo match */}
+            <Link
+              href={`/match/${nextMatchDate}/serie_a`}
+              className="w-full max-w-md p-6 mb-6 text-center bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition-all"
+            >
+              Prossima difesa del titolo
+            </Link>
+          </>
+        ) : (
+          <p>Nessun match disponibile</p>
+        )}
 
         {/* Link alla cronologia */}
         <Link href="/timeline/serie_a" className="w-full max-w-md p-6 text-center bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition-all">
