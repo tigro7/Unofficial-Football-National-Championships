db776a8c0c5779231bd2698dbb3939d5282347df
tigro7 <alby.tiri@gmail.com>
2025-01-12 09:43:06 +0100

    bugfix jersey

--- perennial-leagues/src/app/components/Jersey/index.tsx
+++ perennial-leagues/src/app/components/Jersey/index.tsx
@@ -7,12 +7,8 @@ const Jersey = ({ colors, icon, dimensions }: { colors: { primary: string; secon
 
   const defaultDimensions = dimensions || 16;
   const classString = `w-${defaultDimensions} md:w-${defaultDimensions} lg:w-${defaultDimensions * 2} h-${defaultDimensions} md:h-${defaultDimensions} lg:h-${defaultDimensions * 2} rounded-full`;
-  const iconClassString = `text-center ${defaultDimensions >= 16 ? 'bg-black/10' : ''} relative top-${defaultDimensions / 4} md:top-${defaultDimensions / 4} lg:top-${defaultDimensions / 2} 
-                            left-${defaultDimensions / 4} md:left-${defaultDimensions / 4} lg:left-${defaultDimensions / 2} 
-                            w-${defaultDimensions / 2} md:w-${defaultDimensions / 2} lg:w-${defaultDimensions} 
-                            h-${defaultDimensions / 2} md:h-${defaultDimensions / 2} lg:h-${defaultDimensions}
-                            pt-2 md:pt-2 lg:pt-4 rounded-full`;
-  const minWidthFAIcon = dimensions ? dimensions * 64 : 1024;
+  const iconClassString = `text-center ${defaultDimensions >= 16 ? 'bg-black/10' : ''} relative top-${defaultDimensions / 4} md:top-${defaultDimensions / 4} lg:top-${defaultDimensions / 2} left-${defaultDimensions / 4} md:left-${defaultDimensions / 4} lg:left-${defaultDimensions / 2} w-${defaultDimensions / 2} md:w-${defaultDimensions / 2} lg:w-${defaultDimensions} h-${defaultDimensions / 2} md:h-${defaultDimensions / 2} lg:h-${defaultDimensions} pt-2 md:pt-2 lg:pt-4 rounded-full`;
+  const minWidthFAIcon = defaultDimensions * 64;
   return (
     <div
       className={classString}
