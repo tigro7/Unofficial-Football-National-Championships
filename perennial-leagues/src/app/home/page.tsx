5bf69a159dba25add99bc0e185940e9c6c717e44
tigro7 <alby.tiri@gmail.com>
2025-02-15 15:39:48 +0100

    home bugfix

--- perennial-leagues/src/app/home/page.tsx
+++ perennial-leagues/src/app/home/page.tsx
@@ -66,7 +66,7 @@ const HomePage = () => {
             </span> 
             <span className='w-full lg:w-1/2 mt-[var(--margin-big)] block'>
                 <Button buttonLink={`/${lastMatchLeague}/match/${lastMatchNumber}`} buttonText='Last match' primary className='mr-1' />
-                <Button buttonLink={`/${lastMatchLeague}/${reigningChampion}`} buttonText={isLoading ? 'Crowning a champ...' : reigningChampion ? `${reigningChampion} is the reigning champion` : 'Uh oh! Something is missing...'} primary={false} />
+                <Button buttonLink={`/${lastMatchLeague}/team/${reigningChampion}`} buttonText={isLoading ? 'Crowning a champ...' : reigningChampion ? `${reigningChampion} is the reigning champion` : 'Uh oh! Something is missing...'} primary={false} />
             </span>
             <div className="flex flex-wrap justify-between w-full mt-[var(--margin-huge)]">
                 <Card 
