7db8c94101effca2031b6b7f6e80a6cf8d9b200e
tigro7 <alby.tiri@gmail.com>
2025-02-25 16:19:35 +0100

    feat: updated post indentation to improve readability and to include links.

--- perennial-leagues/src/app/blog/[post]/post.tsx
+++ perennial-leagues/src/app/blog/[post]/post.tsx
@@ -21,13 +21,46 @@ const League = ({post_id} : { post_id: number}) => {
         fetchBlogPost();
     }, [post_id]);
 
+    const renderParagraph = (paragraph: string, index: number) => {
+        const linkRegex = /\(url:'(.*?)';testo:'(.*?)'\)/g;
+        const parts = [];
+        let lastIndex = 0;
+        let match;
+    
+        while ((match = linkRegex.exec(paragraph)) !== null) {
+          // Aggiungi il testo prima del link
+          if (match.index > lastIndex) {
+            parts.push(paragraph.substring(lastIndex, match.index));
+          }
+          // Aggiungi il link
+          parts.push(
+            <a key={match.index} href={match[1]} className="link-secondary" target="_blank" rel="noopener noreferrer">
+              {match[2]}
+            </a>
+          );
+          lastIndex = linkRegex.lastIndex;
+        }
+    
+        // Aggiungi il testo dopo l'ultimo link
+        if (lastIndex < paragraph.length) {
+          parts.push(paragraph.substring(lastIndex));
+        }
+    
+        return (
+          <p key={index} className="mb-4">
+            {parts}
+          </p>
+        );
+    };
+    
+
     return (
         <>
             <span className='par w-1/2'>{blogPost?.date ? new Date(blogPost.date).toLocaleDateString() : ''}</span>
             <h3 className="h3 w-1/2">{blogPost?.title}</h3>
             <span className='par w-1/2'>{blogPost?.author}</span>
-            <span className="h4 w-1/2 text-primary mt-[var(--margin-md)] block">
-                {blogPost?.body}
+            <span className="h4 w-1/2 lg:w-full text-primary mt-[var(--margin-md)] block">
+                {blogPost?.body.split('\n').map((paragraph, index) => renderParagraph(paragraph, index))}
             </span>
         </>
       );
